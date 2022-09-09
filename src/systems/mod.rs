0c67ac17666ceb81e3275838307b68bf36e8241d
Nick Baez <w0m85k27@protonmail.com>
2022-09-09 11:57:45 -0400

    updated movement system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,6 +2,7 @@ mod collision;
 mod end_turn;
 mod entity_render;
 mod map_render;
+mod movement;
 mod player_input;
 mod random_move;
 use crate::prelude::*;
@@ -18,6 +19,8 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(collision::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
@@ -30,7 +33,7 @@ pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
         .flush()
-        .add_system(collision::collisions_system())
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
